1e874d1ff67abf5fd4bd1491251ee893f58b1304
Michal Bielawski <michal.bielawski02@gmail.com>
2024-02-16 20:52:37 +0100

    first commit

---
+++ tictactoe.rb
@@ -0,0 +1,12 @@
+module TicTacToe
+    LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+    
+    class Game
+        def def initialize(player_1_class, player_2_class)
+            @board = Array.new(10)
+
+            @current_player_id = 0
+            @players = []
+        end
+    end
+end
